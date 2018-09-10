30f12c3f3aa5b16455dd4c8f972779d273b83b3f
rbarbazz <rbarbazz@student.42.fr>
2018-09-10 12:30:43 +0200

    store sfile created

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -6,12 +6,28 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 11:24:59 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/09/10 11:26:48 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/09/10 12:14:59 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASM_H
 # define ASM_H
 # include "libft.h"
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+
+typedef struct		s_sfile
+{
+	char			*line;
+	struct s_file	*next;
+}					t_sfile;
+
+/*
+** store sfile
+*/
+
+int	store_sfile(char *filename, t_sfile **sfile);
 
 #endif
--- asm/src/main.c
+++ asm/src/main.c
@@ -6,7 +6,7 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 11:24:26 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/09/10 11:33:59 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/09/10 12:07:20 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,29 @@
 
 static int	display_usage(char *prog_name)
 {
-	ft_printf("Usage: %s [-a] <sourcefile.s>\n    -a : Instead of creating a\
-	 .cor file, outputs a stripped and annotated version of the code to the\
-	  standard output\n", prog_name);
+	ft_printf("Usage: %s [-a] <sourcefile.s>\n    -a : Instead of creating a \
+.cor file, outputs a stripped and annotated version of the code to the \
+standard output\n", prog_name);
 	return (EXIT_FAILURE);
+}
+
+static char	*check_args(int argc, char **argv)
+{
+	if (argc < 2 || argc > 3)
+		return (NULL);
+	if (argc == 3 && ft_strcmp(argv[1], "-a") != 0)
+		return (NULL);
+	return (argv[argc - 1]);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
+	t_sfile	*sfile;
+	char	*filename;
+
+	sfile = NULL;
+	if (!(filename = check_args(argc, argv)))
 		return (display_usage(argv[0]));
+	store_sfile(filename, &sfile);
 	return (EXIT_SUCCESS);
 }
