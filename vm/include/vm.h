3523a885e5ae6d53201157beb64e7f71083c398c
Mikael SAMAK <msamak@student.42.fr>
2018-09-26 16:47:18 +0200

    initialisation structure

--- vm/include/vm.h
+++ vm/include/vm.h
@@ -6,7 +6,7 @@
 /*   By: msamak <msamak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 13:22:30 by msamak            #+#    #+#             */
-/*   Updated: 2018/09/26 13:37:37 by msamak           ###   ########.fr       */
+/*   Updated: 2018/09/26 16:39:54 by msamak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,31 @@
 # include "../libft/include/libft.h"
 # include "op.h"
 
+typedef struct			s_process
+{
+	char				reg[REG_NUMBER][REG_SIZE];
+	short				pc;
+	int					carry;
+	struct s_process	*next;
+}						t_process;
+
+typedef struct			s_player
+{
+	char				*name;
+	char				*comment;
+}						t_player;
+
+typedef struct			s_global
+{
+	char				map[MEM_SIZE];
+	t_process			*process;
+	t_player			player_tab[MAX_PLAYERS];
+}						t_global;
+
+/*
+** Get the Glabal Structure
+*/
+
+t_global				*get_global(void);
 
 #endif
