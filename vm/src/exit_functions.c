8637017ee90a8ba832306eca7ea0cdf6a5daa56e
Mikael SAMAK <msamak@e1r6p12.42.fr>
2018-10-02 19:13:53 +0200

    parsing++;

--- vm/src/exit_functions.c
+++ vm/src/exit_functions.c
@@ -6,11 +6,29 @@
 /*   By: msamak <msamak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 17:32:26 by msamak            #+#    #+#             */
-/*   Updated: 2018/09/27 19:09:52 by msamak           ###   ########.fr       */
+/*   Updated: 2018/10/02 19:09:23 by msamak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+
+void		free_player(void)
+{
+	t_global *info;
+	t_player *tmp;
+	t_player *save;
+
+	info = get_global();
+	tmp = info->player;
+	while (tmp)
+	{
+		save = tmp->next;
+		ft_strdel(&tmp->name);
+		ft_strdel(&tmp->comment);
+		ft_memdel((void **)&tmp);
+		tmp = save;
+	}
+}
 
 void		exit_corewar(int error_code)
 {
@@ -28,8 +46,14 @@ void		exit_corewar(int error_code)
 		exit(READ_FILE_ERROR);
 	if (error_code == MALLOC_ERROR)
 	{
-		ft_printf("Malloc : Error - [Protected]");
+		ft_printf("Malloc : Error - [Protected]\n");
 		exit(MALLOC_ERROR);
 	}
+	if (error_code == INVALID_MAGIC)
+	{
+		ft_printf("Invalid COREWAR_EXEC_MAGIC\n");
+		exit(INVALID_MAGIC);
+	}
+	free_player();
 	exit(0);
 }
