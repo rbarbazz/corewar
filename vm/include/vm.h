04f23aa2f8be0170232c65d7878d0114ed6a16df
rbarbazz <rbarbazz@student.42.fr>
2018-10-05 18:49:17 +0200

    vendredi soir tu connais

--- vm/include/vm.h
+++ vm/include/vm.h
@@ -6,7 +6,7 @@
 /*   By: msamak <msamak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/26 13:22:30 by msamak            #+#    #+#             */
-/*   Updated: 2018/10/05 16:57:29 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/10/05 18:47:58 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@
 # define NO_CHAMP 7
 # define INVALID_CLOSE_FD 8
 
-# define FILE_LEN_MAX PROG_NAME_LENGTH + COMMENT_LENGTH + CHAMP_MAX_SIZE + 12
+# define FILE_LEN_MAX	PROG_NAME_LENGTH + COMMENT_LENGTH + CHAMP_MAX_SIZE + 12
 
 /*
 ** *****************************************************************************
@@ -51,23 +51,23 @@
 ** *****************************************************************************
 */
 
-# define RESET   "\033[0m"
-# define BLACK   "\033[30m"      /* Black */
-# define RED     "\033[31m"      /* Red */
-# define GREEN   "\033[32m"      /* Green */
-# define YELLOW  "\033[33m"      /* Yellow */
-# define BLUE    "\033[34m"      /* Blue */
-# define MAGENTA "\033[35m"      /* Magenta */
-# define CYAN    "\033[36m"      /* Cyan */
-# define WHITE   "\033[37m"      /* White */
-# define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-# define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-# define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-# define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-# define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-# define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-# define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-# define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+# define RESET			"\033[0m"
+# define BLACK			"\033[30m"
+# define RED			"\033[31m"
+# define GREEN			"\033[32m"
+# define YELLOW			"\033[33m"
+# define BLUE			"\033[34m"
+# define MAGENTA		"\033[35m"
+# define CYAN			"\033[36m"
+# define WHITE			"\033[37m"
+# define BOLDBLACK  	"\033[1m\033[30m"
+# define BOLDRED    	"\033[1m\033[31m"
+# define BOLDGREEN  	"\033[1m\033[32m"
+# define BOLDYELLOW 	"\033[1m\033[33m"
+# define BOLDBLUE   	"\033[1m\033[34m"
+# define BOLDMAGENTA	"\033[1m\033[35m"
+# define BOLDCYAN   	"\033[1m\033[36m"
+# define BOLDWHITE  	"\033[1m\033[37m"
 
 /*
 ** *****************************************************************************
@@ -160,7 +160,6 @@ void					exit_corewar(int error_code);
 int						open_file(char *filename);
 char					*read_file(int fd, char *filename);
 void					close_file(int fd);
-
 
 /*
 ** *****************************************************************************
