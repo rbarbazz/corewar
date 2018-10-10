6c4da30f26e1da73899c28fee9c80eba908d2e0f
xperrin <xperrin@student.42.fr>
2018-10-10 23:43:19 +0200

    Fully working callback events and restructured gtk struct

--- vm/src/gui/main_gui.c
+++ vm/src/gui/main_gui.c
@@ -1,17 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main_gui.c                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
+/*   main_gui.c                                         :+:      :+:    :+:   */ /*                                                    +:+ +:+         +:+     */
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/10 16:07:02 by xperrin           #+#    #+#             */
-/*   Updated: 2018/10/10 19:41:43 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/10/10 23:21:05 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 #include "gui.h"
+
+
+void		callback_test(GtkToggleButton *tbutton, gpointer data)
+{
+	ft_putendl("it works!");
+}
 
 /*
 ** Initialize the GTK info struct and load the glade file
@@ -20,6 +25,7 @@
 static int	builder_and_glad_init(t_gtkinfo *i)
 {
 	ft_bzero(i, sizeof(t_gtkinfo));
+	ft_bzero(&i->w, sizeof(t_gtkwin));
 	i->builder = gtk_builder_new();
 	i->gladefilename = g_build_filename("corewar.glade", NULL);
 	gtk_builder_add_from_file(i->builder,
@@ -27,6 +33,7 @@ static int	builder_and_glad_init(t_gtkinfo *i)
 	g_free(i->gladefilename);
 	if (i->err)
 	{
+		ft_putstr_fd("Error: Problem loading gladle file\n", 2);
 		g_error_free(i->err);
 		return (0);
 	}
@@ -45,11 +52,11 @@ int			main(int ac, char **av)
 	gtk_init(&ac, &av);
 	if (!builder_and_glad_init(&i))
 		return (1);
-	i.w_m = GTK_WIDGET(gtk_builder_get_object(i.builder, MAIN_WIN));
-	i.w_a = GTK_WIDGET(gtk_builder_get_object(i.builder, ABOUT_WIN));
-	gtk_widget_show_all(i.w_m);
-	g_signal_connect(G_OBJECT(i.w_m), "destroy", (GCallback)gtk_main_quit, NULL);
-	gtk_widget_show_all(i.w_a);
+	gtk_builder_connect_signals(i.builder, NULL);
+	i.w.m = GTK_WIDGET(gtk_builder_get_object(i.builder, MAIN_WIN));
+	i.w.a = GTK_WIDGET(gtk_builder_get_object(i.builder, ABOUT_WIN));
+	gtk_widget_show_all(i.w.m);
 	gtk_main();
 	return (0);
+	free_all();
 }
