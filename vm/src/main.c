46873d5ec489e720a22e417ee6594a9b4a2cff92
rbarbazz <rbarbazz@student.42.fr>
2018-10-10 17:25:09 +0200

    functions init moved

--- vm/src/main.c
+++ vm/src/main.c
@@ -6,35 +6,11 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/10 11:24:26 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/10/10 11:08:59 by msamak           ###   ########.fr       */
+/*   Updated: 2018/10/10 17:24:32 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
-
-static void	init_global(t_global *info, char *prog_name)
-{
-	info->prog_name = prog_name;
-	info->process_head = NULL;
-	info->process_tail = NULL;
-	info->process_count = 0;
-	info->player = NULL;
-	info->player_count = 0;
-	info->map = NULL;
-	info->clock.cycle = 0;
-	info->clock.cycle_to_die = CYCLE_TO_DIE;
-	info->clock.current_cycle = 0;
-	info->clock.dump = 0;
-	info->visual = 0;
-	info->dump = 0;
-}
-
-t_global	*get_global(void)
-{
-	static t_global info;
-
-	return (&info);
-}
 
 /*
 ** *****************************************************************************
