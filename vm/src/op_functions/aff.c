e6bc4711efe508358a96a3c36186bbbb9a109bf3
Mikael SAMAK <msamak@student.42.fr>
2018-10-11 15:07:09 +0200

    lld added

--- vm/src/op_functions/aff.c
+++ vm/src/op_functions/aff.c
@@ -6,7 +6,7 @@
 /*   By: msamak <msamak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/10 15:39:18 by msamak            #+#    #+#             */
-/*   Updated: 2018/10/11 13:29:29 by msamak           ###   ########.fr       */
+/*   Updated: 2018/10/11 14:37:09 by msamak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void		aff(t_global *info, t_process *process)
 	else
 		process->carry = 0;
 	ft_printf("un process dit : %c\n", process->reg[process->curr_op.param[0] - 1] % 256);
+	ft_printf("un process dit : %u\n", process->reg[process->curr_op.param[0] - 1] % 256);
 	if (info)
 		return ;
 }
--- vm/src/op_functions/lld.c
+++ vm/src/op_functions/lld.c
@@ -6,7 +6,7 @@
 /*   By: msamak <msamak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/11 11:37:54 by msamak            #+#    #+#             */
-/*   Updated: 2018/10/11 13:23:15 by msamak           ###   ########.fr       */
+/*   Updated: 2018/10/11 14:51:28 by msamak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 
 void		lld(t_global *info, t_process *process)
 {
+	char			*tmp;
+	unsigned int	value;
+
 	if (process->type_param[0] == T_IND)
 	{
-
+		tmp = get_value_at_position(info->map, process->curr_op.param[0], IND_SIZE);
+		value = tab_to_int(tmp);
+		ft_strdel(&tmp);
+		process->reg[process->curr_op.param[1] - 1] = value;
 	}
 	else if (process->type_param[0] == T_DIR)
 		process->reg[process->curr_op.param[1] - 1] = process->curr_op.param[0];
@@ -24,6 +30,4 @@ void		lld(t_global *info, t_process *process)
 		process->carry = 1;
 	else
 		process->carry = 0;
-	if (info)
-		return ;
 }
