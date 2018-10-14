070ab00bd365b4ca5e2138342a7c8968a657046a
rbarbazz <rbarbazz@student.42.fr>
2018-10-15 00:59:08 +0200

    debug added

--- vm/src/op_functions/add.c
+++ vm/src/op_functions/add.c
@@ -6,11 +6,17 @@
 /*   By: msamak <msamak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/10 13:28:53 by msamak            #+#    #+#             */
-/*   Updated: 2018/10/14 11:41:11 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/10/14 20:38:46 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+
+/*
+** *****************************************************************************
+** adds value in p0 with value in p1 and store in p2
+** *****************************************************************************
+*/
 
 void	add(t_global *info, t_process *process)
 {
@@ -25,6 +31,7 @@ void	add(t_global *info, t_process *process)
 		return ;
 	if (check_reg(info, process, 2))
 		return ;
+	ft_printf("	add r%d + r%d -> r%d\n", process->curr_op.param[0], process->curr_op.param[1], process->curr_op.param[2]);
 	process->reg[process->curr_op.param[2] - 1] = param0 + param1;
 	if (!(process->reg[process->curr_op.param[2] - 1]))
 		process->carry = 1;
