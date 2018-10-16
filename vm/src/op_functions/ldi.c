7692b86adb2678ad4dcee7860a79f182fce663e6
rbarbazz <rbarbazz@student.42.fr>
2018-10-16 13:29:52 +0200

    commit before merge visu

--- vm/src/op_functions/ldi.c
+++ vm/src/op_functions/ldi.c
@@ -6,7 +6,7 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/14 15:22:01 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/10/16 12:17:17 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/10/16 13:01:06 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,9 @@ void	ldi(t_global *info, t_process *process)
 		param0 %= IDX_MOD;
 	if (process->type_param[1] == T_IND)
 		param1 %= IDX_MOD;
-	ft_printf("	ldi r%d -> %d %d\n", process->curr_op.param[0], param1, param2);
-	ft_printf("	-> store to %d + %d = %d", param1, param2, param1 + param2);
-	ft_printf(" (with pc and mod %d)\n", (((param1 + param2) % IDX_MOD) + process->op_pos));
+	//ft_printf("	ldi r%d -> %d %d\n", process->curr_op.param[0], param1, param2);
+	//ft_printf("	-> store to %d + %d = %d", param1, param2, param1 + param2);
+	//ft_printf(" (with pc and mod %d)\n", (((param1 + param2) % IDX_MOD) + process->op_pos));
 	value = get_value_at_position(info->map, ((param0 + param1) % IDX_MOD) + process->op_pos, 4);
 	process->reg[process->curr_op.param[2] - 1] = tab_to_int(value);
 	ft_strdel(&value);
