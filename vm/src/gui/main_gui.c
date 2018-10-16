c6e204f6abf16dfdb5bd3c41789910fe7b422e0c
xperrin <xperrin@student.42.fr>
2018-10-16 18:19:54 +0000

    Thread safe way to update the GUI (no direct gtk call) Getting there was annoying AS FUCK

--- vm/src/gui/main_gui.c
+++ vm/src/gui/main_gui.c
@@ -6,7 +6,7 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/10 16:07:02 by xperrin           #+#    #+#             */
-/*   Updated: 2018/10/14 17:58:35 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/10/16 16:23:30 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,13 +69,16 @@ static int	gui_struct_init(t_gtkinfo *i)
 
 	/* signal handling & objects get */
 	GtkWidget		*w_mem_txt;
+	GtkWidget		*w_play_txt;
 
 	gtk_builder_connect_signals(i->builder, i);
 	i->w.m = GTK_WIDGET(gtk_builder_get_object(i->builder, MAIN_WIN));
 	i->w.a = GTK_WIDGET(gtk_builder_get_object(i->builder, ABOUT_WIN));
 
 	w_mem_txt = GTK_WIDGET(gtk_builder_get_object(i->builder, MEM_TXT_VIEW));
+	w_play_txt = GTK_WIDGET(gtk_builder_get_object(i->builder, PLAY_TXT_VIEW));
 	i->t.mem = gtk_text_view_get_buffer(GTK_TEXT_VIEW(w_mem_txt));
+	i->t.play = gtk_text_view_get_buffer(GTK_TEXT_VIEW(w_play_txt));
 
 	gtk_widget_show_all(i->w.m);
 	return (1);
