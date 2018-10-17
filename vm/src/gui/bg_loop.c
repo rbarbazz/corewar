4fe1df24aa2e72a78b74185e20dbee95961709e2
xperrin <xperrin@student.42.fr>
2018-10-17 15:08:41 +0200

    GTK vm loop start

--- vm/src/gui/bg_loop.c
+++ vm/src/gui/bg_loop.c
@@ -6,24 +6,43 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/14 16:31:52 by xperrin           #+#    #+#             */
-/*   Updated: 2018/10/16 18:17:59 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/10/17 15:07:20 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gui.h"
 #include <time.h>
 
+int			play_gtk(t_gtkinfo *i)
+{
+	i->vm->process_count = i->vm->player_count;
+	create_initial_process(i->vm);
+	/* if (!i->vm->visual) */
+	/* 	display_intro(i->vm); */
+	while (!cycle(i->vm) && i->vm->process_count)
+	{
+		check_process(i->vm);
+		update_map(i->vm);
+		if (i->vm->visual)
+			print_map(i->vm);
+		//Debug
+		//	usleep(500);
+	}
+	/* get_winner(i->vm); */
+	return (0);
+}
+
 static void		u_text_players(t_gtkinfo *i)
 {
 	int			len;
-	GtkTextIter	end;
+	GtkTextIter	*end = NULL;
 	t_player *p;
 
 	len = ft_strlen(i->vm->player->name);
 
 	gtk_text_buffer_set_text(i->t.play, "Player 1\nName: ", -1);
-	gtk_text_buffer_get_end_iter(i->t.play, &end);
-	gtk_text_buffer_insert(i->t.play, &end
+	gtk_text_buffer_get_end_iter(i->t.play, end);
+	gtk_text_buffer_insert(i->t.play, end
 			, i->vm->player->name, -1);
 }
 
@@ -42,7 +61,7 @@ void			*bg_loop(t_gtkinfo *i)
 				*/
 				write_player_in_map(i->vm);
 				gdk_threads_add_idle(u_text_players, i);
-				play(i->vm);
+				play_gtk(i->vm);
 				i->b.run = 0;
 			}
 			else
