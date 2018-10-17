6f463b5d4f392bf5f9d309601681ee1f5d4929cc
Mikael SAMAK <msamak@e1r9p19.42.fr>
2018-10-17 12:17:19 +0200

    commit de mort qui tue

--- vm/src/op_functions/ldi.c
+++ vm/src/op_functions/ldi.c
@@ -6,7 +6,7 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/14 15:22:01 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/10/16 13:01:06 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/10/17 11:53:09 by msamak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	ldi(t_global *info, t_process *process)
 	int		param0;
 	int		param1;
 	char	*value;
+	short	param_sum;
 
 	param0 = 0;
 	param1 = 0;
@@ -26,16 +27,16 @@ void	ldi(t_global *info, t_process *process)
 		return ;
 	if (check_reg(info, process, 2))
 		return ;
-	if (process->type_param[0] == T_IND)
-		param0 %= IDX_MOD;
-	if (process->type_param[1] == T_IND)
-		param1 %= IDX_MOD;
-	//ft_printf("	ldi r%d -> %d %d\n", process->curr_op.param[0], param1, param2);
-	//ft_printf("	-> store to %d + %d = %d", param1, param2, param1 + param2);
-	//ft_printf(" (with pc and mod %d)\n", (((param1 + param2) % IDX_MOD) + process->op_pos));
-	value = get_value_at_position(info->map, ((param0 + param1) % IDX_MOD) + process->op_pos, 4);
+	param_sum = (short)param0 + (short)param1;
+	value = get_value_at_position(info->map, param_sum % IDX_MOD + process->op_pos, 4);
 	process->reg[process->curr_op.param[2] - 1] = tab_to_int(value);
 	ft_strdel(&value);
+	if (info->debug)
+	{
+		ft_printf("P	%d |	ldi %hd %hd r%d\n", process->process_nb, param0, param1, process->curr_op.param[2]);
+		ft_printf("	-> load from %hd + %hd = %hd", param0 , param1, param0 + param1);
+		ft_printf(" (with pc and mod %hd)\n",((param_sum) % IDX_MOD + process->op_pos));
+	}
 	if (!(process->reg[process->curr_op.param[2] - 1]))
 		process->carry = 1;
 	else
--- vm/src/op_functions/lldi.c
+++ vm/src/op_functions/lldi.c
@@ -6,7 +6,7 @@
 /*   By: msamak <msamak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/11 15:24:38 by msamak            #+#    #+#             */
-/*   Updated: 2018/10/14 15:32:32 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/10/17 12:16:21 by msamak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,17 @@ void	lldi(t_global *info, t_process *process)
 		return ;
 	if (check_reg(info, process, 2))
 		return ;
-	value = get_value_at_position(info->map, param0 + param1 +\
+	value = get_value_at_position(info->map, (short)param0 + (short)param1 +\
 	process->op_pos, 4);
+	//ft_printf("param 0 = %d ===> %d\n", (short)process->curr_op.param[0], param0);
 	process->reg[process->curr_op.param[2] - 1] = tab_to_int(value);
 	ft_strdel(&value);
+	if (info->debug)
+	{
+		ft_printf("P	%d |	lldi %d %d r%d\n", process->process_nb, param0, param1, process->curr_op.param[2]);
+		ft_printf("	-> load from %d + %d = %d", param0 , param1, param0 + param1);
+		ft_printf(" (with pc %d)\n", param0 + param1 + process->op_pos);
+	}
 	if (!(process->reg[process->curr_op.param[2] - 1]))
 		process->carry = 1;
 	else
