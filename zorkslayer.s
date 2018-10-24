9ffdb49a77e3c3dc7576e172a1dfe07059bf66d9
rbarbazz <rbarbazz@student.42.fr>
2018-10-24 12:14:22 +0200

    zorkslayer last version

--- zorkslayer.s
+++ zorkslayer.s
@@ -5,67 +5,12 @@
 ## extremely efficient against zork ##
 ######################################
 
-stairs:
-	st r2, -7
-	st r2, -17
-	st r2, -27
-	st r2, -37
-	st r2, -47
-	st r2, -57
-	st r2, -67
-	st r2, -77
-	st r2, -87
-	st r2, -97
-	st r2, -107
-	st r2, -117
-	st r2, -127
-	st r2, -137
-	st r2, -147
-	st r2, -157
-	st r2, -167
-	st r2, -177
-	st r2, -187
-	st r2, -197
-	st r2, -207
-	st r2, -217
-	st r2, -227
-	st r2, -237
-	st r2, -247
-	st r2, -257
-	st r2, -267
-	st r2, -277
-	st r2, -287
-	st r2, -297
-	st r2, -307
-	st r2, -317
-	st r2, -327
-	st r2, -337
-	st r2, -347
-	st r2, -357
-	st r2, -367
-	st r2, -377
-	st r2, -387
-	st r2, -397
-	st r2, -407
-	st r2, -417
-	st r2, -427
-	st r2, -437
-	st r2, -447
-	st r2, -457
-	st r2, -467
-	st r2, -477
-	st r2, -487
-	st r2, -497
-	st r2, -507
-
 	sti	r1, %:live, %1
 
 live:
 	live %1
-
-	add	r6, r6, r6
-	fork %:writenb
-	zjmp %:live
+	add	r8, r8, r8
+	fork %:live
 
 writenb:
 	sti	r1, %:live2, %1
@@ -81,6 +26,7 @@ writenb:
 	sti	r1, %:live12, %1
 	sti	r1, %:live13, %1
 
+	st r2, -340
 
 live2:
 	live %1
@@ -106,8 +52,15 @@ live12:
 	live %1
 
 	ld %57673475, r7
-	st r7, 364
-	live13:
-		live %1
+	st r7, 361
+	st r7, 292
+	st r7, 223
+	st r7, 154
+	st r7, 85
+	st r7, 16
+
+
+live13:
+	live %1
 
 #by rbarbazz
