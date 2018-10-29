af63b5f7d2050ba04e30eb44238e9dc71264c8ee
rbarbazz <rbarbazz@student.42.fr>
2018-10-29 21:00:28 +0100

    segfault fixed... probably last commit

--- vm/src/op_functions/st.c
+++ vm/src/op_functions/st.c
@@ -6,11 +6,17 @@
 /*   By: rbarbazz <rbarbazz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/12 16:32:33 by rbarbazz          #+#    #+#             */
-/*   Updated: 2018/10/27 13:33:45 by rbarbazz         ###   ########.fr       */
+/*   Updated: 2018/10/29 20:59:37 by rbarbazz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+
+static void	print_debug(t_global *info, t_process *process)
+{
+	if (info->debug)
+		ft_printf(" %d\n", process->curr_op.param[1]);
+}
 
 /*
 ** *****************************************************************************
@@ -21,22 +27,23 @@
 ** *****************************************************************************
 */
 
-void	st(t_global *info, t_process *process)
+void		st(t_global *info, t_process *process)
 {
 	int		param0;
 	short	param1;
 
 	param0 = 0;
 	param1 = process->curr_op.param[1];
-	if (get_param_value(info, process, 0, &param0))
+	if (get_param_value(info, process, 0, &param0)\
+		|| (process->type_param[1] == T_REG\
+			&& check_reg(info, process, 1)))
 		return ;
 	if (info->debug)
 		ft_printf("P%5d | st r%d", process->process_nb,\
 			process->curr_op.param[0]);
 	if (process->type_param[1] == T_REG)
 	{
-		if (info->debug)
-			ft_printf(" %d\n", process->curr_op.param[1]);
+		print_debug(info, process);
 		process->reg[process->curr_op.param[1] - 1] = param0;
 	}
 	else
@@ -44,7 +51,7 @@ void	st(t_global *info, t_process *process)
 		if (info->debug)
 			ft_printf(" %d\n", param1);
 		param1 %= IDX_MOD;
-		write_at_position(info->map, process, process->op_pos + param1,\
-			param0);
+		write_at_position(info->map, process, process->op_pos\
+			+ param1, param0);
 	}
 }
