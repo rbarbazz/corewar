b40cbc2b696cdf5ee305c9b664ecfb50f2291c02
xperrin <xperrin@student.42.fr>
2018-10-30 01:07:35 +0100

    gtk_color_button_get_rgba is deprecated, FUCK YOU GTK

--- vm/src/gui/call_colors.c
+++ vm/src/gui/call_colors.c
@@ -6,15 +6,21 @@
 /*   By: xperrin <xperrin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/29 23:57:36 by xperrin           #+#    #+#             */
-/*   Updated: 2018/10/30 00:02:54 by xperrin          ###   ########.fr       */
+/*   Updated: 2018/10/30 00:44:51 by xperrin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "gui.h"
 
-void	callback_colorset_p1(GtkColorButton *widget, t_gtkinfo *i)
+void	callback_colorset(GtkColorButton *widget, t_gtkinfo *i)
 {
-	(void)widget;
+	if (widget == i->c.color_p1)
+		ft_putendl("yooO! p1");
+	else if (widget == i->c.color_p2)
+		ft_putendl("yooO! p2");
+	else if (widget == i->c.color_p3)
+		ft_putendl("yooO! p3");
+	else
+		ft_putendl("yooO! p4");
 	(void)i;
-	ft_putendl("yooO!");
 }
